1b615060e33ac1ec3e469478fee7eba34db50d3e
zlargon <zlargon1988@gmail.com>
2019-07-05 13:15:31 -0400

    [app] connect android library with android app

--- app/src/main/java/com/zlargon/hello/example/MainActivity.java
+++ app/src/main/java/com/zlargon/hello/example/MainActivity.java
@@ -2,6 +2,8 @@ package com.zlargon.hello.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import com.zlargon.hello.hellosdk.HelloSDK;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Hello(View v) {
+        HelloSDK.sayHello();
+    }
+
 }
